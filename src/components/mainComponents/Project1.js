63d723795836fd44d20cd4605c84f9345fa36944
svvishjs <andreww92@live.com>
2021-01-10 15:57:08 +0000

    looked at adding hover effects on skills but didn't like it so deleted.

--- src/components/mainComponents/Project1.js
+++ src/components/mainComponents/Project1.js
@@ -4,9 +4,11 @@ const Project1 = () => {
 	return (
 		<>
 			<article className="brng">
-				<h2 className="brng-h2">
+				{/* <h2 className="brng-h2">
 					BRNG <span className="brng-h2-span">Sports</span>
-				</h2>
+				</h2> */}
+
+				<h2>BRNG Sports</h2>
 				<img className="brng-img" src="./assets/brng.jpg" alt="" />
 			</article>
 		</>
--- src/components/mainComponents/Skills.js
+++ src/components/mainComponents/Skills.js
@@ -4,31 +4,31 @@ const Skills = () => {
 	return (
 		<>
 			<h2 className="skills-h2">Skills</h2>
-			<div class="skills-container">
+			<div className="skills-container">
 				<div className="skills-grid">
-					<div class="row1-skill1">
+					<div className="row1-skill1">
 						<img src="./assets/js.jpg" alt="javascript-logo" />
 					</div>
-					<div class="row1-skill2">
+					<div className="row1-skill2">
 						<img src="./assets/html.webp" alt="html-logo" />
 					</div>
 
-					<div class="row2-skill1">
+					<div className="row2-skill1">
 						<img src="./assets/react.png" alt="react-logo" />
 					</div>
-					<div class="row2-skill2">
+					<div className="row2-skill2">
 						<img src="./assets/css.webp" alt="css-logo" />
 					</div>
-					<div class="row3-skill1">
+					<div className="row3-skill1">
 						<img src="./assets/vue.png" alt="vue-logo" />
 					</div>
-					<div class="row3-skill2">
+					<div className="row3-skill2">
 						<img src="./assets/mongo.webp" alt="mongo-logo" />
 					</div>
-					<div class="row4-skill1">
+					<div className="row4-skill1">
 						<img src="./assets/python.webp" alt="python-logo" />
 					</div>
-					<div class="row4-skill2">
+					<div className="row4-skill2">
 						<img src="./assets/java.webp" alt="java-logo" />
 					</div>
 				</div>
