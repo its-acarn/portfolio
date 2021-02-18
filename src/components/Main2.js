ccfae030026d1ae04a64b09eb267331f2ddbaa82
its-acarn <andreww92@live.com>
2021-02-18 16:32:58 +0000

    added scroll speed to projects

--- src/components/Main2.js
+++ src/components/Main2.js
@@ -8,63 +8,80 @@ import About from './mainComponents/About';
 import '../styles/main2.css';
 
 const Main2 = () => {
-
 	const [width, setWidth] = useState(window.innerWidth);
 	const breakpoint = 1024;
 
 	const changeScroll = (scroll) => {
-		(width > breakpoint) ? scroll.direction = 'horizontal' : scroll.direction = 'vertical';
-		(width > breakpoint) ? scroll.options.direction = 'horizontal' : scroll.options.direction = 'vertical';
+		width > breakpoint
+			? (scroll.direction = 'horizontal')
+			: (scroll.direction = 'vertical');
+		width > breakpoint
+			? (scroll.options.direction = 'horizontal')
+			: (scroll.options.direction = 'vertical');
 	};
-  
+
 	useEffect(() => {
 		const scroll = new LocomotiveScroll({
 			el: document.querySelector('[data-scroll-container]'),
 			smooth: true,
 			direction: 'horizontal',
 			scrollFromAnywhere: true,
-			reloadOnContextChange: true,
+			reloadOnContextChange: true
 		});
 
 		function handleResize() {
-      setWidth(window.innerWidth);
+			setWidth(window.innerWidth);
 			changeScroll(scroll);
-			console.log(scroll)
-
+			console.log(scroll);
 		}
-    window.addEventListener('resize', handleResize)
+		window.addEventListener('resize', handleResize);
 
-    return _ => {
-      window.removeEventListener('resize', handleResize)
-		}
-
-	})
+		return (_) => {
+			window.removeEventListener('resize', handleResize);
+		};
+	});
 
 	return (
-			<>
-					<main className="content" data-scroll-container>
-						<div className="gallery">
-							<section className="gallery-item detail">
-								<Detail />
-							</section>
-							<section className="gallery-item scroll">
-								<Scroll />
-							</section>
-							<section className="gallery-item about" data-scroll data-scroll-speed="3">
-								<About />
-							</section>
-							<section className="gallery-item skills" data-scroll data-scroll-speed="3">
-								<Skills />
-							</section>
-							<section className="gallery-item projects-title" data-scroll data-scroll-speed="2">
-								<h1 className="projects-h1">PROJECTS...</h1>
-							</section>
-							<section className="gallery-item projects">
-								<Projects />
-							</section>
-						</div>
-					</main>
-			</>
+		<>
+			<main className="content" data-scroll-container>
+				<div className="gallery">
+					<section className="gallery-item detail">
+						<Detail />
+					</section>
+					<section className="gallery-item scroll">
+						<Scroll />
+					</section>
+					<section
+						className="gallery-item about"
+						data-scroll
+						data-scroll-speed="3"
+					>
+						<About />
+					</section>
+					<section
+						className="gallery-item skills"
+						data-scroll
+						data-scroll-speed="3"
+					>
+						<Skills />
+					</section>
+					<section
+						className="gallery-item projects-title"
+						data-scroll
+						data-scroll-speed="2"
+					>
+						<h1 className="projects-h1">PROJECTS...</h1>
+					</section>
+					<section
+						className="gallery-item projects"
+						data-scroll
+						data-scroll-speed="3"
+					>
+						<Projects />
+					</section>
+				</div>
+			</main>
+		</>
 	);
 };
 
--- src/components/mainComponents/Projects.js
+++ src/components/mainComponents/Projects.js
@@ -3,14 +3,13 @@ import Shelv from './Shelv.js';
 import Geoexplorers from './Geoexplorers.js';
 import '../../styles/projects.css';
 
-
 const Projects = () => {
 	return (
-			<div className="projects-grid">
-				<Brng />
-				<Geoexplorers />
-				<Shelv />
-			</div>
+		<div className="projects-grid">
+			<Brng />
+			<Geoexplorers />
+			<Shelv />
+		</div>
 	);
 };
 export default Projects;
